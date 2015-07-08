7591acd2bffaa2229afce9d4da13569385cd2745
Joris Vanhecke <joris@jorisvanhecke.be>
2015-07-08 21:58:09 +0200

    iOS-Patch
    Apple iOS Safari doesn't support ```background-size: cover;```.
    This is a workaround.

--- javascripts/y.js
+++ javascripts/y.js
@@ -16,10 +16,38 @@
 
 
 $( document ).ready(function() {
-  console.log('ready');
+  var iOS = ( navigator.userAgent.match(/iPad|iPhone|iPod/g) ? true : false );
+  console.log('[*]');
   var windowH = $(window).height(),
+  windowW = $(window).width(),
   introEl = $('div.intropic'),
   introHeadingH = introEl.height();
 
-  introEl.css('padding', (windowH - introHeadingH)/2 + 'px 0');
+  if(iOS){
+    $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'css/ios.css') );
+    $('.segment').each(function(index){
+      var image_url = $(this).css('background-image'), image;
+      console.log(index);
+      // Remove url() or in case of Chrome url("")
+      image_url = image_url.match(/^url\("?(.+?)"?\)$/);
+
+      if (image_url[1]) {
+        image_url = image_url[1];
+        image = new Image();
+
+        // just in case it is not already loaded
+        $(image).load(function () {
+            //alert(image.height/(image.width/windowsW));
+
+            console.log(image.height/(image.width/windowW));
+
+            $($('.intropic')[index]).css('padding', (image.height/(image.width/windowW))/2 + 'px 0');
+        });
+
+        image.src = image_url;
+      }
+    });
+  } else {
+      introEl.css('padding', (windowH - introHeadingH)/2 + 'px 0');
+  }
 });
